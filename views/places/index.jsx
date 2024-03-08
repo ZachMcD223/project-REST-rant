7c76d69d527bc22e0a7fa048b1b4827d6c934cd4
ZachMcD223 <148635518+ZachMcD223@users.noreply.github.com>
2024-03-07 19:56:41 -0600

    New places can be added

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -21,5 +21,18 @@ const Def = require('../default')
           </Def>
       )
       }
-      
+      let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]})
+      res.render('places/index', { places })
       module.exports = index
