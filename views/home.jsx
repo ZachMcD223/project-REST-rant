c12009f0fd38b9f6519eec5ccff1cafc693b1062
ZachMcD223 <148635518+ZachMcD223@users.noreply.github.com>
2024-03-11 13:36:43 -0500

    Adding styles to rest-rant

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,11 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src="/images/food.jpg" alt="food" />
+               
+              Photo by <a href="https://unsplash.com/@ben_brunner?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Benjamin Brunner</a> on <a href="https://unsplash.com/photos/red-tomato-beside-green-vegetable-on-white-table-bAcMAhWciiM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+              </div>
               <a href="/places">
   <button className="btn-primary">Places Page</button>
 </a>
