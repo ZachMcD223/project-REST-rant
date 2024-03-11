95521986aa1ed3b36aee6b8f416b91cf3ef1f57e
ZachMcD223 <148635518+ZachMcD223@users.noreply.github.com>
2024-03-11 13:04:58 -0500

    Adding styles to rest-rant

--- views/default.jsx
+++ views/default.jsx
@@ -1,17 +1,17 @@
 const React = require('react')
-const Def = require('../default')
 
 function Def (html) {
     return (
         <html>
             <head>
                 <title>Title</title>
+                <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
                 {html.children}
             </body>
         </html>
     )
-  }
-  
-  module.exports = Def
+}
+
+module.exports = Def
